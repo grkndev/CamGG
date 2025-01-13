2dc4884449b12f497418ecba1134143e862a64f5
GweepCreative <gweepcreative@gmail.com>
2025-01-13 18:56:46 +0300

    CameraView & DevClient

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,20 +3,43 @@ import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import * as ScreenOrientation from "expo-screen-orientation";
 import Icon from "@/components/Icons";
+import { CameraView, CameraType, useCameraPermissions } from "expo-camera";
 
 export default function Index() {
+  const [facing, setFacing] = React.useState<CameraType>("front");
+  const [permission, requestPermission] = useCameraPermissions();
+
   React.useEffect(() => {
     ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
   }, []);
+
+  if (!permission) {
+    return <View />;
+  }
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View>
+        <Text>We need your permission to show the camera</Text>
+        <TouchableOpacity onPress={requestPermission}>
+          <Text>grant permission</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
     <SafeAreaView className="bg-primary flex-1">
       <View className="flex flex-row items-center justify-center w-full h-full">
         <View className=" w-[90%] h-full p-4 ">
           <View className="relative rounded-xl overflow-hidden border-red-500 border-2">
-            <Image
-              className="w-full h-full"
-              source={require("../../assets/images/test.png")}
-            />
+            <View className="w-full h-full">
+              <CameraView
+                facing={facing}
+                className="flex-1"
+              ></CameraView>
+            </View>
+
             <View className="absolute p-2 flex flex-row items-center justify-center gap-2">
               <View className="bg-red-500 py-1 px-1.5 flex flex-row items-center justify-center gap-1 rounded-lg">
                 <View className="bg-white h-2 w-2 rounded-full animate-[ping-re__2s_ease-in-out_infinite] " />
